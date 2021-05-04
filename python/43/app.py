f6becf0b79c74728b2029049c56d9467b19127a1
DenaKrull <denakrull@gmail.com>
2021-05-04 19:08:05 -0400

    app.py

---
+++ python/43/app.py
@@ -0,0 +1,18 @@
+print("Hello World")
+x = 5
+print(x, type(x))
+x = 'Hello'
+print(x, type(x))
+y = True
+print(y, type(y))
+z = 1.2
+print(z, type(z))
+# this is a comment
+
+my_large_variable_name = 5
+my_very_long_int = 5767843984738725817584074872487542805942
+print(my_very_long_int, type(my_very_long_int))
+
+print(id(my_very_long_int))
+my_very_long_int +=1
+print(id(my_very_long_int))
