5770c47a252f384112f6f4cae1da5dffa3fad3d4
DenaKrull <denakrull@gmail.com>
2021-12-12 23:24:26 -0500

    class 85

---
+++ JavaScript/85/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const json5 = require('json5');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset',
+
+            },
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader']
+            },
+
+
+        ]
+    }
+};
